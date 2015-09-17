a8d84b7d86f781561c86eb21853e7bd0baa1a814
Markus Boese <mboese@wolterskluwer.de>
2015-09-17 16:12:36 +0200

    add function for gitignore.io.

--- zsh/zsh.symlink/git.sh
+++ zsh/zsh.symlink/git.sh
@@ -1,12 +1,2 @@
-# git
-#alias gs='git status'
-#alias gd='git diff'
-#alias glog='git log'
-#alias gca='git commit -v -a'
-#alias gl='git pull'
-#alias gp='git push'
-#alias gpb='git push --set-upstream origin HEAD' # Push local branch to remote
-#alias gco='git checkout'
-#alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
-#alias gb='git branch -a'
 alias gclean='git clean -xfd'
+function gi() { curl -L -s https://www.gitignore.io/api/$1 ;}
